0cffe7aece42cc8a758231f9e9b802452f7950f5
Oleh Abanin <juristoleh@gmail.com>
2022-11-29 00:40:06 +0200

    fix: review controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -13,6 +13,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewModel } from './review.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { ReviewService } from './review.service';
+import { REVIEW_NOT_FOUND } from './review.constants';
 
 @Controller('review')
 export class ReviewController {
@@ -20,17 +21,19 @@ export class ReviewController {
 
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
-    this.reviewService.create(dto);
+    return this.reviewService.create(dto);
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deletedDoc = await this.reviewService.delete(id);
     if (!deletedDoc) {
-      throw new HttpException('', HttpStatus.NOT_FOUND);
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
   }
 
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {}
+  async getByProduct(@Param('productId') productId: string) {
+    return this.reviewService.findByProductId(productId);
+  }
 }
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,13 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ReviewModel } from './review.model';
 import { ModelType, DocumentType } from '@typegoose/typegoose/lib/types';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { Types } from 'mongoose';
+import { InjectModel } from 'nestjs-typegoose';
 
 @Injectable()
 export class ReviewService {
   constructor(
-    @Inject(ReviewModel) private readonly reviewModel: ModelType<ReviewModel>,
+    @InjectModel(ReviewModel)
+    private readonly reviewModel: ModelType<ReviewModel>,
   ) {}
 
   async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
