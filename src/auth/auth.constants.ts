5b7d474adc4c303006389bff35e194ad7b8401e4
Oleh Abanin <juristoleh@gmail.com>
2022-12-26 14:03:17 +0200

    fix: repo

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'User with same email already exists';
+export const USER_DOES_NOT_EXIST = 'User with this email does not exist';
+export const WRONG_PASSWORD = 'WRONG password';
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,9 @@
+import { PassportStrategy } from '@nestjs/passport';
+
+export class JwtStrategy extends PassportStrategy() {
+  constructor() {
+    super();
+  }
+
+  async validate();
+}
