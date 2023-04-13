ede8d1ba5936c7f0c28a37df955d81648485f62d
Oleh Abanin <oleh@outvio.com>
2023-04-13 14:13:15 +0300

    fix: nestjs

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,14 +2,27 @@ import {Controller, HttpCode, Post, UploadedFile, UseGuards, UseInterceptors} fr
 import {FileInterceptor} from "@nestjs/platform-express";
 import {JwtAuthGuard} from "../auth/guards/jwt.guard";
 import {FileElementResponse} from "./dto/file-element.response";
+import {FilesService} from "./files.service";
+import {MFile} from "./mfile.class";
+
 
 @Controller('files')
 export class FilesController {
+
+    constructor(private readonly FilesService: FilesService) {
+    }
+
     @Post('upload')
     @HttpCode(200)
     @UseInterceptors(FileInterceptor('files'))
     @UseGuards(JwtAuthGuard)
-    async uploadFile(@UploadedFile() file: Express.Multer.File ): Promise<FileElementResponse[]> {
-
+    async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
+        const saveArray: MFile[] = [new MFile(file)];
+        //const saveArray: MFile[] = [file];
+        if (file.mimetype.includes('image')) {
+            const buffer = await this.FilesService.convertToWebp(file.buffer);
+            saveArray.push(new MFile({originalname:`${file.originalname.split('.')[0]}.webp`, buffer }));
+        }
+        return this.FilesService.saveFiles(saveArray);
     }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {FileElementResponse} from "./dto/file-element.response";
-import format from 'date-fns/format';
+import {format} from 'date-fns';
+import {ensureDir, writeFile} from "fs-extra";
 import {path} from "app-root-path";
+import * as sharp from "sharp";
+import {MFile} from "./mfile.class";
+
 @Injectable()
 export class FilesService {
-    async saveFiles(file:Express.Multer.File):Promise<FileElementResponse[]>{
+    async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
+        //async saveFiles(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
         const dateFolder = format(new Date(), 'yyyy-MM-dd');
-        const uploadFolder = `${path}/uploads/`
+        const uploadFolder = `${path}/uploads/${dateFolder}`
+        await ensureDir(uploadFolder);
+        const res: FileElementResponse[] = [];
 
+        for (const file of files) {
+            await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer)
+            res.push({url: `${dateFolder}/${file.originalname}`, name: file.originalname})
+        }
+        return res;
+
+    }
+
+    convertToWebp(file: Buffer):Promise<Buffer>{
+        return sharp(file).webp().toBuffer()
     }
 }
